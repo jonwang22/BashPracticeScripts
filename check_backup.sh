75f429b03b44a64e8fa0cd366a5d7916e0a5a06a
Jon <jonwang.kuralabs@gmail.com>
2024-08-13 22:39:23 +0000

    Adding ls to script

--- check_backup.sh
+++ check_backup.sh
@@ -6,8 +6,11 @@
 DIR="backup"
 
 if [[ -d "$DIR" ]]; then
-      echo "Directory $DIR exists in the current directory."
+      echo "Directory $DIR exists in the current directory. Removing that directory."
+      rmdir $DIR
+      ls -al | grep -i $DIR
 else
       echo "$DIR does not exist. Creating 'backup' directory in current directory."
       mkdir $DIR
+      ls -al | grep -i $DIR
 fi      
