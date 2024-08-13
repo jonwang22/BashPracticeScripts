f9e5d2c362dc0d019f450ff9b816eae4229b33a3
Jon <jonwang.kuralabs@gmail.com>
2024-08-13 22:53:31 +0000

    updating script

--- check_backup.sh
+++ check_backup.sh
@@ -7,7 +7,6 @@ DIR="backup"
 
 if [[ -d "$DIR" ]]; then
       echo "Directory $DIR exists in the current directory. Removing that directory."
-      rmdir $DIR
       ls -al | grep -i $DIR
 else
       echo "$DIR does not exist. Creating 'backup' directory in current directory."
