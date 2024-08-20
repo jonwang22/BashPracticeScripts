c5cad861cf6b8b391c3a824a810ac88e49ee91c8
Jon Wang <155007142+jonwang22@users.noreply.github.com>
2024-08-20 15:24:22 -0400

    Updating comments in script

--- systemsinfo.sh
+++ systemsinfo.sh
@@ -37,7 +37,7 @@ function display_menu () {
         echo ""
 }
 
-# Creating menu select case function
+# Creating menu select case function that runs the designated function when valid option is selected.
 function menu_selection () {
 	case $selection in
                 1)
@@ -90,7 +90,7 @@ function system_recheck () {
         done
 }
 
-# For the IP Address, we will need an echo statement and two variables. These two variables for Private and Public IP Addresses. There should be a command we can run that will give this to us and we will then need to isolate that output and assign it to the variables for the output for the user.
+# Checking IP, grabbing Private and Public IP and storing them into variables. ifconfig command will give us the private IP, for public IP we will need to query an external dns. There's multiple ways to grab public IP. 
 function check_ip () {
 	private_ip=$(ifconfig | grep inet | head -1 | awk '{print $2}')
 	public_ip=$(curl ifconfig.me 2>/dev/null) # Without the 2>/dev/null, we would see the stderr output when the script executes. We don't want that so we're redirecting that output into /dev/null. We can also use $(dig +short myip.opendns.com @resolver1.opendns.com) here.
@@ -102,7 +102,7 @@ function check_user () {
 	echo "You are $USER."
 }
 
-# For the CPU Information, there should be a command that displays how many CPUs the system has, based on that output we'll have to use a combination of grep and awk to isolate the information we want to then echo into a sentence to display to the user.
+# For CPU we're going to check the information found in /proc/cpuinfo. Searching for cores and awking the output to grab the number of CPU cores the system has.
 function cpu_info () {
 	cpu_core=$(cat /proc/cpuinfo | grep cores | awk '{print $4}') #This lets us find how many cpu cores are in our system.
 	echo "The system has $cpu_core CPU(s)."
@@ -115,21 +115,26 @@ function mem_info () {
 	echo "There is $mem_unused Mebibytes of unused memory of out of the total $mem_total Mebibytes of memory on your system."
 }
 
-# For top 5 memory, we'll need to figure out the ps aux options to use to create a table output of processes sorted by memory usage. pmem, pid, cmd. There should be a formatting solution with in the ps command with the different options. We'll pair that with head to grab the top 5.
+# For top 5 memory processes, we'll use ps with -eo flags. 'e' checks every process, 'o' allows for formatting.
+# Here we're formatting our output to show process memory used, process id, and the command that resource is tied to.
+# We sort it through via pmem, then we sort the output to get the processes in descending order and we take the top 5 processes. We use head -6 to account for the header in the output.
 function top_mem_processes () {
 	echo "Here are the top 5 processes using memory right now."
 	echo ""
 	ps -eo pmem,pid,cmd --sort pmem | sort -r | head -6
 }
 
-# For top 5 cpu, we'll need to do the same as memory but sort it based on CPU usage so our columns displayed will change from pmem to pcpu most likely or some equivalent showing the cpu usage. We'll use sort and head here for top 5.
+# For top 5 cpu processes, we'll use ps with -eo flags. Same as Memory above.
+# Here we're formatting our output to show process cpu used, process id, and the command that resource is tied to.
+# We sort it through via pcpu, then we sort the output to get the processes in descending order and we take the top 5 processes. We use head -6 to account for the header in the output.
 function top_cpu_processes () {
 	echo "Here are the top 5 processes with the most CPU Utilization right now."
 	echo " "
 	ps -eo pcpu,pid,cmd --sort pcpu | sort -r | head -6
 }
 
-# For Network Connectivity, we grab user input for website or IP address and assign it to a variable. use that variable in ping command, grab the output which should be the last line of the ping output and just output that. Not sure if we need to use redirection or not. Will need to refamiliarize myself with the output format.
+# For network connectivity, it's a bit more involved. We'll need to validate the user input is a valid website url or ip address.
+# We have a function validating that user input and then we have a function performing the logical check on the input to make sure we have connection.
 function validate_website () {
 	local input=$1
 	
@@ -164,6 +169,7 @@ function validate_website () {
 	fi
 }
 
+# Here is where our logic happens after we've validated the user input.
 function network_connect () {
 	read -p "Enter a website or IP address: " website
 	echo "Checking validity of website or IP address....."
@@ -172,9 +178,9 @@ function network_connect () {
 		echo ""
 		echo "Proceeding with network connectivity check..."
 		echo ""
-		ping_output=$(ping $website -c 10)
-        	packet_loss=$(echo "$ping_output" | grep -i "packet loss" | awk -F "," '{gsub(/^ */, "", $3); print $3}')
-        	time=$(echo "$ping_output" | tail -1 | awk -F "/" '{print $5}')
+		ping_output=$(ping $website -c 10) # Pinging website to check what our RoundTrip Time looks like
+        	packet_loss=$(echo "$ping_output" | grep -i "packet loss" | awk -F "," '{gsub(/^ */, "", $3); print $3}') # We then grep for packet loss line to isolate that value into a variable for our echo output for the user. gsub is being used to remove the leading whitespace after awk.
+        	time=$(echo "$ping_output" | tail -1 | awk -F "/" '{print $5}') # Here we're grabbing the average time metric for each ping found within the RTT stat
         	echo "It took $time ms to connect to '$website' and there was $packet_loss."
 	else
 		echo ""
