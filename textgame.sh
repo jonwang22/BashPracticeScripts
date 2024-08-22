163423bcd6781302a8b2f7a2e90f6511cdd6a64c
Jon Wang <155007142+jonwang22@users.noreply.github.com>
2024-08-22 16:03:49 -0400

    Finalizing text

--- textgame.sh
+++ textgame.sh
@@ -104,7 +104,29 @@ function tavern_choice () {
             tavern_choice
             ;;
     esac
+}
 
+function confront_choice () {
+    buffer
+    echo -e "1) Call the guards for help \n2) Attack Docsilius\n3) Give up"
+    read -p "Select where you want to go: " confront_choice
+    buffer
+    case $confront_choice in
+        1) 
+            guards
+            ;;
+        2)
+            attack
+            ;;
+        3)  
+            give_up
+            ;;
+        *)
+            echo -e "You're kinda stuck here....so call for help, fight, or give up...."
+            buffer
+            confront_choice
+            ;;
+    esac
 }
 
 # All the different destination options will be under here for option selects in menu.
@@ -144,8 +166,39 @@ function tavern () {
     tavern_choice
 }
 
-function mountain() {
-    
+function mountain () {
+    echo -e "****   You head to the mountain. It's a dangerous trek to get there.\n****   You're almost at the base and you see a cave."
+    buffer
+    speaker "Docsilius"
+    echo -e "This doesn't bode well for us $player....Do we go in to the cave? The jewel might be there?"
+    pause
+    echo -e "****   You decide to go in the cave"
+    pause
+    speaker "Docsilius"
+    echo -e "Watch out $player!!!!"
+    pause
+    echo -e "****   All of a sudden the mountain starts to shake, you hear a roar in the distance.\n****   The rumbling gets louder and louder and you see a titan emerge.\n****   The titan is very angry and proceeds to swing his club at you.\n****   You do what you can to survive but you have so many injuries.\n****   You see Docsilius laying down in a pool of red liquid.\n****   He's gone...You look up and as soon as your head lifts up, your vision goes dark.\n****   You hit the ground hard....you died....\n.....GAME OVER....."
+}
+
+function confront () {
+    echo -e "****   You head to the Castle to confront King Mont.\n****   The Jewel was with him all along but he's the one asking for adventurers to find it.\n****   You barge in demanding to see the Golden Jewel in the King's Chest.\n****   King Mont looks stunned."
+    buffer
+    speaker "King Mont"
+    echo -e "What is the meaning of this?! What do you think you're doing!\nThis is the King's Hall! How dare you barge in with this behavior! GUARDS!"
+    buffer
+    echo -e "****   The Royal Guards swarm you and take you to the dungeon.\n****   Days pass....You don't know how long you've been imprisoned.\n****   All of a sudden you see Docsilius pull out jewel from his sleeve.\n****   He chuckles while holding it, staring at you."
+    pause
+    speaker "Docsilius"
+    echo -e "You fool. I've had the jewel this whole time. Now that I have this in my possession...I AM THE NEW RULER OF TEXTERRIA! BOW DOWN BEFORE ME!!!!"
+    confront_choice
+}
+
+function guards () {
+    echo -e "You decide to call the guards. You're screaming for help.\nDocsilius walks behind you as your screaming and knocks you out.\nYou wake up and you're in your room.\nIt was all a dream but you look down and find the Golden Jewel in your hand.\nWas it a dream?\n.....GAME OVER....."
+}
+
+function attack () {
+    echo -e "You decide to fight. You swing at Docsilius and he dodges your attack.\nHe's quicker than you thought. You attack again.\nAs you attack your hand hits the wall.\nDocsilius swings at you but you barely avoid his punch.\nYou try kicking him one last time with all your might.\nYou land a blow, Docsilius flies back and hits the wall.\nAs he hits the wall, his head hits a rock on the wall that's jutted out.\nDocsilius falls down gasping, holding the back of his head.\nThen he falls limp.....\nYou take the Golden Jewel from him.\nGuards finally arrive after hearing all the ruckus.\nThey see you've killed Docsilius for the Jewel.\nYou're imprisoned forever.....\nYou go to sleep and you wake up. You notice you're in your bedroom.\nIt was all a dream...or was it?\nYou look down in your hand and you have the Golden Jewel...\n.....GAME OVER....."
 }
 
 function run_away () {
@@ -159,6 +212,10 @@ function run_away () {
     echo "...."
     sleep 3
     echo -e "You start screaming and running for the hills.\nYou get a good distance away and you feel freedom, then all of a sudden....\n*BAM* *WHACK* *PHWAPOOM*\nYou trip on a tree root sticking out of the ground, you fall and hit your head on a rock.\nYou perish....for running from your destiny.\n.....GAME OVER....."
+}
+
+function give_up () {
+    echo -e "You decide to give up. You don't care anymore about the journey.\nYou look at Docsilius and just go to sleep.\nYou awake and you find yourself in your room. It was all just a bad dream.\n.....GAME OVER....."
 }
 # Main game time
 introduction
