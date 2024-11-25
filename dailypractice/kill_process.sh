5c218100813b229f0432b1843581307aa3bd1ed8
Jon Wang <jonwang.kuralabs@gmail.com>
2024-11-25 16:18:34 +0000

    modifying a bit

--- dailypractice/kill_process.sh
+++ dailypractice/kill_process.sh
@@ -3,7 +3,10 @@
 # $ ./kill_process.sh
 # Listing all running processes:
 # PID COMMAND
-# 1234 nginx 5678 apache2 91011 bash 121314 python3 Enter the PID of the process you want to kill: 1234 Killing process with PID 1234... Process 1234 (nginx) has been successfully killed.
+# 1234 nginx 5678 apache2 91011 bash 121314 python3 
+# Enter the PID of the process you want to kill: 1234 
+# Killing process with PID 1234... 
+# Process 1234 (nginx) has been successfully killed.
 # $ ./kill_process.sh
 # Listing all running processes:
 # PID COMMAND
@@ -13,3 +16,4 @@
 # 121314 python3
 # Enter the PID of the process you want to kill: 9999
 # Error: No process with PID 9999 found. Please try again.
+
