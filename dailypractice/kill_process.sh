5cd69b79a54e90b3edeaf8a5802887ec138171c6
Jon Wang <jonwang.kuralabs@gmail.com>
2024-11-25 16:37:09 +0000

    adding first attempt

--- dailypractice/kill_process.sh
+++ dailypractice/kill_process.sh
@@ -3,7 +3,10 @@
 # $ ./kill_process.sh
 # Listing all running processes:
 # PID COMMAND
-# 1234 nginx 5678 apache2 91011 bash 121314 python3 
+# 1234 nginx 
+# 5678 apache2 
+# 91011 bash 
+# 121314 python3 
 # Enter the PID of the process you want to kill: 1234 
 # Killing process with PID 1234... 
 # Process 1234 (nginx) has been successfully killed.
@@ -17,3 +20,14 @@
 # Enter the PID of the process you want to kill: 9999
 # Error: No process with PID 9999 found. Please try again.
 
+#!/bin/bash
+
+echo "Listing all running processes: "
+ps -ro pid,comm
+
+read -p "Enter the PID of the process you want to kill: " process
+echo "Killing process with PID $process...."
+if kill "$process" 2>dev/null; then
+    echo "Process $process has been successfully killed."
+else
+    echo "Error: No process with PID $process found. Please try again."
