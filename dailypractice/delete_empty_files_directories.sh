58b730746a73f21b6a73469b60335c0b0e1a13b3
Jon Wang <jonwang.kuralabs@gmail.com>
2024-11-25 16:49:48 +0000

    changing format

--- dailypractice/delete_empty_files_directories.sh
+++ dailypractice/delete_empty_files_directories.sh
@@ -2,6 +2,20 @@
 
 # Output Example: 
 
-# $ ./delete_empty.sh Empty files and directories found: 1. Empty file: /path/to/empty_file.txt 2. Empty directory: /path/to/empty_directory Do you want to delete these files and directories? (yes/no): yes Deleting... File '/path/to/empty_file.txt' has been deleted. Directory '/path/to/empty_directory' has been deleted.
-# $ ./delete_empty.sh Empty files and directories found: 1. Empty file: /path/to/empty_file.txt 2. Empty directory: /path/to/empty_directory Do you want to delete these files and directories? (yes/no): no No files or directories were deleted.
-# $ ./delete_empty.sh No empty files or directories found.
+# $ ./delete_empty.sh 
+# Empty files and directories found: 1. 
+# Empty file: /path/to/empty_file.txt 2. 
+# Empty directory: /path/to/empty_directory 
+# Do you want to delete these files and directories? (yes/no): yes 
+# Deleting... File '/path/to/empty_file.txt' has been deleted. 
+# Directory '/path/to/empty_directory' has been deleted.
+
+# $ ./delete_empty.sh 
+# Empty files and directories found: 1. 
+# Empty file: /path/to/empty_file.txt 2. 
+# Empty directory: /path/to/empty_directory 
+# Do you want to delete these files and directories? (yes/no): no 
+# No files or directories were deleted.
+
+# $ ./delete_empty.sh 
+# No empty files or directories found.
